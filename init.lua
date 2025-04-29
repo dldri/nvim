51bb5d423d5f382800d4bf2f4034d902a53f2207
dldri <diovian.liu@gmail.com>
2025-04-29 15:20:03 +0800

    updated color overides for catppuccin-mocha theme

--- init.lua
+++ init.lua
@@ -829,8 +829,6 @@ require('lazy').setup({
         color_overrides = {
           mocha = {
             base = '#000000',
-            mantle = '#000000',
-            crust = '#000000',
           },
         },
         integrations = {
