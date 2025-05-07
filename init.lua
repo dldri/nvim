57d249a6a2464ccef6fa34fca039d09458ed3a60
dldri <diovian.liu@gmail.com>
2025-05-07 18:51:45 +0800

    updated init.lua. started splitting config files

--- init.lua
+++ init.lua
@@ -1,30 +1,16 @@
 --[[
-
-    TODO:
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    TODO:
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-    NOTE:
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-NOTE:
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
+--
+-- NOTE:
+-- MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
+-- which is very useful when you're not exactly sure of what you're looking for.
+--
+-- NOTE:
+-- If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
+--
 --]]
 
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+-- NOTE: [[ Must be loaded first. Contains set leader. ]]
+require 'codri.set'
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
