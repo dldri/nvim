97fa466d99e644710c1af39adc0554dc1996f05b
dldri <diovian.liu@gmail.com>
2025-05-10 09:30:52 +0800

    more remaps

--- lua/dldri/remap.lua
+++ lua/dldri/remap.lua
@@ -38,4 +38,11 @@ vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 
+-- Centres cursor while searching
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Allows pasting over something
+vim.keymap.set('x', '<leader>p', '"_deP')
+
 -- ===== ===== ===== ===== =====
