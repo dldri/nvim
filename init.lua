0bb4bfb5f0c9df01325dd644524a0f89338a6f83
Dldri <diovian.liu@gmail.com>
2025-07-12 12:43:38 +0800

    split lazydev.nvim

--- init.lua
+++ init.lua
@@ -29,19 +29,6 @@ vim.opt.rtp:prepend(lazypath)
 
 -- NOTE: Setup lazy.nvim
 require('lazy').setup({
-  -- LSP Plugins
-  {
-    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        -- Load luvit types when the `vim.uv` word is found
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-      },
-    },
-  },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
