29976d22866377414a9da4395d98413e06d8bf34
Dldri <diovian.liu@gmail.com>
2025-07-21 18:04:05 +0800

    added biome for all supported ft instead of prettierd

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -14,7 +14,7 @@ return {
     },
   },
   opts = {
-    notify_on_error = false,
+    notify_on_error = true,
     format_on_save = function(bufnr)
       local disable_filetypes = { c = true, cpp = true }
       if disable_filetypes[vim.bo[bufnr].filetype] then
@@ -28,13 +28,8 @@ return {
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
-      javascript = { 'prettierd' },
-      typescript = { 'prettierd' },
-      javascriptreact = { 'prettierd' },
-      typescriptreact = { 'prettierd' },
       css = { 'prettierd' },
       html = { 'prettierd' },
-      json = { 'prettierd' },
       markdown = { 'prettierd' },
     },
   },
