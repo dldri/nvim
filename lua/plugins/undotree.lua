a92a2fd341547b4ade5b87accecff67ff8ed089b
dldri <diovian.liu@gmail.com>
2025-07-23 15:11:06 +0800

    added undotree compatibility between windows and wsl

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,7 +1,14 @@
 return {
   'mbbill/undotree',
   config = function()
-    vim.g.undotree_DiffCommand = 'FC'
+    local is_wsl = vim.fn.has 'wsl' == 1
+
+    if is_wsl then
+      vim.g.undotree_DiffCommand = 'diff'
+    else
+      vim.g.undotree_DiffCommand = 'FC'
+    end
+
     vim.g.undotree_WindowLayout = 4
     vim.g.undotree_SetFocusWhenToggle = 1
     vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Toggle [U]ndotree' })
