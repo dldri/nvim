96d83e92931a45b5b18e4acb37f62fec860b37b4
dldri <diovian.liu@gmail.com>
2025-07-26 04:31:33 +0800

    updated vim-fugitive keymaps

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,4 +1,21 @@
 return {
   'tpope/vim-fugitive',
-  config = function() end,
+  keys = {
+    { '<leader>gs', '<cmd>G<CR>', desc = '[g]it [s]tatus' },
+    { '<leader>gw', '<cmd>Gwrite<CR>', desc = 'Stage file' },
+    { '<leader>gc', '<cmd>G commit<CR>', desc = '[g]it [c]ommit' },
+    { '<leader>gd', '<cmd>Gdiffsplit<CR>', desc = '[g]it [d]iff' },
+    { '<leader>gb', '<cmd>G blame<CR>', desc = '[g]it [b]lame' },
+    { '<leader>gl', '<cmd>G lg<CR>', desc = '[g]it [l]og' },
+    { '<leader>gp', '<cmd>G push<CR>', desc = '[g]it [p]ush' },
+    { '<leader>gP', '<cmd>G pull<CR>', desc = '[g]it [P]ull' },
+  },
+  config = function()
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'fugitive*', 'git' },
+      callback = function()
+        vim.keymap.set('n', 'q', '<cmd>q<CR>', { buffer = true, desc = '[q]uit fugitive map' })
+      end,
+    })
+  end,
 }
