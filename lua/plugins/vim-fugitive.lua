97329cc8d0762e3622c6bc07f9f3551f495c8f58
dldri <diovian.liu@gmail.com>
2025-07-27 06:46:32 +0800

    updated fugitive with better mappings

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -3,20 +3,80 @@ return {
   event = 'VimEnter',
   keys = {
     { '<leader>gs', '<cmd>G<CR>', desc = 'git [s]tatus' },
-    { '<leader>gw', '<cmd>Gwrite<CR>', desc = 'Stage file' },
-    { '<leader>gc', '<cmd>G commit<CR>', desc = 'git [c]ommit' },
     { '<leader>gd', '<cmd>Gdiffsplit<CR>', desc = 'git [d]iff' },
-    { '<leader>gb', '<cmd>G blame<CR>', desc = 'git [b]lame' },
+    { '<leader>gB', '<cmd>G blame<CR>', desc = 'git [B]lame' },
     { '<leader>gl', '<cmd>G lg<CR>', desc = 'git [l]og' },
-    { '<leader>gp', '<cmd>G push<CR>', desc = 'git [p]ush' },
-    { '<leader>gP', '<cmd>G pull<CR>', desc = 'git [P]ull' },
-    { '<leader>gf', '<cmd>G fetch<CR>', desc = 'git [f]etch' },
+    { '<leader>gb', '<cmd>G branch<CR>', desc = 'git [b]ranch' },
   },
   config = function()
-    vim.api.nvim_create_autocmd('FileType', {
-      pattern = { 'fugitive*', 'git' },
+    local Dldri_Fugitive = vim.api.nvim_create_augroup('Dldri_Fugitive', {})
+    local autocmd = vim.api.nvim_create_autocmd
+
+    autocmd('FileType', {
+      group = Dldri_Fugitive,
+      pattern = 'fugitive*',
       callback = function()
-        vim.keymap.set('n', 'q', '<cmd>q<CR>', { buffer = true, desc = '[q]uit fugitive map' })
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = { buffer = bufnr, remap = false }
+
+        opts.desc = 'git [p]ush'
+        vim.keymap.set('n', '<leader>gp', function()
+          vim.cmd.Git 'push'
+        end, opts)
+
+        opts.desc = 'git [p]ull'
+        vim.keymap.set('n', '<leader>gP', function()
+          vim.cmd.Git 'pull'
+        end, opts)
+
+        opts.desc = 'git [c]ommit'
+        vim.keymap.set('n', '<leader>gc', function()
+          vim.cmd.Git 'commit'
+        end, opts)
+      end,
+    })
+
+    autocmd('FileType', {
+      group = Dldri_Fugitive,
+      pattern = 'git',
+      callback = function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = { buffer = bufnr, remap = false }
+
+        opts.desc = 'git [f]etch'
+        vim.keymap.set('n', '<leader>gf', function()
+          vim.cmd.Git 'fetch'
+        end, opts)
+      end,
+    })
+
+    autocmd('User', {
+      group = Dldri_Fugitive,
+      pattern = 'FugitivePager',
+      callback = function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = { buffer = bufnr, remap = false }
+
+        opts.desc = 'Delete branch under cursor'
+        vim.keymap.set('n', 'X', function()
+          local branch = vim.fn.expand '<cfile>'
+          vim.ui.input({ prompt = 'Delete branch ' .. branch .. '? (y/n): ' }, function(input)
+            if input and input:lower() == 'y' then
+              vim.cmd.Git('branch -D ' .. branch)
+              vim.cmd 'bdelete'
+              vim.cmd.Git 'branch'
+            else
+              vim.notify('Branch deletion cancelled', vim.log.levels.INFO)
+            end
+          end)
+        end, opts)
+
+        opts.desc = 'Delete branch under cursor'
+        vim.keymap.set('n', '<CR>', function()
+          local branch = vim.fn.expand '<cfile>'
+          vim.cmd.Git('checkout ' .. branch)
+          vim.cmd 'bdelete'
+        end, opts)
       end,
     })
   end,
