4765e4f15501022fc816a12d3649c3a805d9f522
dldri <diovian.liu@gmail.com>
2025-07-28 07:59:16 +0800

    fugitive - moved fetch keymap from log map to commit map

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -19,9 +19,9 @@ return {
         local bufnr = vim.api.nvim_get_current_buf()
         local opts = { buffer = bufnr, remap = false }
 
-        opts.desc = 'git [p]ush'
-        vim.keymap.set('n', '<leader>gp', function()
-          vim.cmd.Git 'push'
+        opts.desc = 'git [f]etch'
+        vim.keymap.set('n', '<leader>gf', function()
+          vim.cmd.Git 'fetch'
         end, opts)
 
         opts.desc = 'git [p]ull'
@@ -29,23 +29,14 @@ return {
           vim.cmd.Git 'pull'
         end, opts)
 
+        opts.desc = 'git [p]ush'
+        vim.keymap.set('n', '<leader>gp', function()
+          vim.cmd.Git 'push'
+        end, opts)
+
         opts.desc = 'git [c]ommit'
         vim.keymap.set('n', '<leader>gc', function()
           vim.cmd.Git 'commit'
-        end, opts)
-      end,
-    })
-
-    autocmd('FileType', {
-      group = Dldri_Fugitive,
-      pattern = 'git',
-      callback = function()
-        local bufnr = vim.api.nvim_get_current_buf()
-        local opts = { buffer = bufnr, remap = false }
-
-        opts.desc = 'git [f]etch'
-        vim.keymap.set('n', '<leader>gf', function()
-          vim.cmd.Git 'fetch'
         end, opts)
       end,
     })
